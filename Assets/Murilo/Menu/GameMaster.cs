557408ee1614df45cf81a2b9d99d6b51e1a9069b
Murilo Sobreiro <murilosobreiro@hotmail.com>
2023-10-07 08:05:14 -0300

    Debug helpers

--- Assets/Murilo/Menu/GameMaster.cs
+++ Assets/Murilo/Menu/GameMaster.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameMaster : MonoBehaviour
 {
     public static GameMaster gameMaster;
+    public Canvas opcoes;
     void Awake()
     {
         DontDestroyOnLoad(transform.gameObject);
@@ -16,7 +17,17 @@ public class GameMaster : MonoBehaviour
         {
             Destroy(gameObject);
         }
-       
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            opcoes.enabled = !opcoes.enabled;
+        }
+    }
+    public void HideOpcoes()
+    {
+        opcoes.enabled = false;
     }
 
 }
